3c9e2f7cf8a1e4918b11c05a8c9c4dcf5cec5b51
Jagroop Singh <geek.jagroop@gmail.com>
2018-04-23 18:59:12 +0530

    sec key updated (more strong)

--- application/config/auth.php
+++ application/config/auth.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['ENABLE_JWT']  = false;
 
-$config['JWT_KEY']     = "59ba36addc2";
+$config['JWT_KEY']     = "4JCum2S253JqZ336ZAJbn6TT";
 
 $config['JWT_ENC_KEY'] = 'sec!ReT423TrexzillaGod*&';
 
